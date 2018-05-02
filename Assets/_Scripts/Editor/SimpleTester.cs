e3366f589d455e137589ce4daa0f41bd204b05c7
dr-matt-smith <dr_matt_smith@me.com>
2018-05-02 09:04:22 +0100

    improved naming

---
+++ Assets/_Scripts/Editor/SimpleTester.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+class SimpleTester
+{
+    [Test]
+    public void TestOnePlusOneEqualsTwo()
+    {
+        // Arrange
+        int n1 = 1;
+        int n2 = 1;
+        int expectedResult = 2;
+
+        // Act
+        int result = n1 + n2;
+
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+}
